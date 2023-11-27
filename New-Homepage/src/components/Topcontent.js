b42fc59013d11bc8fbed45c91ba8e36e6ab1dede
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-11-27 09:45:51 +0000

    top contents readdy for styling

---
+++ New-Homepage/src/components/Topcontent.js
@@ -0,0 +1,43 @@
+export default function Top(){
+    <div className="top-content">
+        <div className="top-left">
+            <img src="" alt="" />
+            <div className="top-left-description">
+                <h2>The Bright Future of Web 3.0?</h2>
+                <div className="description-more">
+                    <p className="left description">We dive into the next
+                        evolution of the web that claims to put the power of
+                        the platforms back into the hands of people. But 
+                        is it really fulfilling its promise?
+                    </p>
+                    <button>READ MORE</button>
+                </div>
+            </div>
+        </div>
+        <div className="top-right">
+            <h3>New</h3>
+            <div className="top-right-description">
+                <h4>Hydrogen VS Electric Cars</h4>
+                <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+                <div className="field-bar"></div>
+            </div>
+
+            <div className="top-right-description">
+                <h4>The Downside of AI Artistry</h4>
+                <p>What are the possible adverse effects of 
+                    on-demand AI image generation?
+                </p>
+                <div className="field-bar"></div>
+            </div>
+
+            <div className="top-right-description">
+                <h4>Is VC Funding Drying Up?</h4>
+                <p>Private funding by VC firms is down 50% YOY. 
+                    We take a look at what that means.
+                </p>
+                <div className="field-bar"></div>
+            </div>
+            
+        </div>
+    </div>
+}
