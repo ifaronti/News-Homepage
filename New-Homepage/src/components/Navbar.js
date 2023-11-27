e8b239d4c78aece1b18fae3c74b0c6c973d73508
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-11-27 08:25:53 +0000

    Navbar component created

---
+++ New-Homepage/src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return(
+        <nav>
+            <h1>W.</h1>
+            <ul>
+                <li>Home</li>
+                <li>New</li>
+                <li>Popular</li>
+                <li>Trending</li>
+                <li>Categories</li>
+            </ul>
+        </nav>
+    )
+}
