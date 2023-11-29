83963b62ac35d03fc918905b6850ff863dc41c51
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-11-29 08:05:06 +0000

    adjusting headers

--- New-Homepage/src/components/Topleft.js
+++ New-Homepage/src/components/Topleft.js
@@ -18,7 +18,7 @@ return(
         <div className="top-left">
             <img className="web3-image" src={`${process.env.PUBLIC_URL}/assets/images/${windowWidth > 700 ? 'image-web-3-desktop.jpg':'image-web-3-mobile.jpg'}`} alt="" />
             <div className="top-left-description">
-                <h2>The Bright Future of Web 3.0?</h2>
+                <h1>The Bright Future of Web 3.0?</h1>
                 <div className="description-more">
                     <p className="left-description">We dive into the next
                         evolution of the web that claims to put the power of
@@ -31,15 +31,15 @@ return(
         </div>
         <div className="top-right">
             <div className="top-right-contents">
-                <h3>New</h3>
+                <h2>New</h2>
                 <div className="top-right-description">
-                    <p className="head">Hydrogen VS Electric Cars</p>
+                    <h3 className="head">Hydrogen VS Electric Cars</h3>
                     <p className="description">Will hydrogen-fueled cars ever catch up to EVs?</p>
                     <div className="field-bar"></div>
                 </div>
 
                 <div className="top-right-description">
-                    <p className="head">The Downsides of AI Artistry</p>
+                    <h3 className="head">The Downsides of AI Artistry</h3>
                     <p className="description">What are the possible adverse effects of 
                         on-demand AI image generation?
                     </p>
@@ -47,7 +47,7 @@ return(
                 </div>
 
                 <div className="top-right-description">
-                    <p className="head">Is VC Funding Drying Up?</p>
+                    <h3 className="head">Is VC Funding Drying Up?</h3>
                     <p className="description">Private funding by VC firms is down 50% YOY. 
                         We take a look at what that means.
                     </p>
