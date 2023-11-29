574dcbfe5c1c1732478b0099173646db802c5651
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-11-29 22:17:42 +0000

    project is done!

--- New-Homepage/src/components/Navbar.js
+++ New-Homepage/src/components/Navbar.js
@@ -5,14 +5,14 @@ export default function Navbar(){
     return(
         <nav>
             <img className="logo" src={`${process.env.PUBLIC_URL}/assets/images/logo.svg`} alt="" />
-            <ul>
-                <li>Home</li>
-                <li>New</li>
-                <li>Popular</li>
-                <li>Trending</li>
-                <li>Categories</li>
-            </ul>
-            <img src={`${process.env.PUBLIC_URL}/assets/images/${showMenu ? 'icon-menu-close.svg':'icon-menu.svg'}`} alt="" />
+                <ul className={showMenu ? 'showLinks':'hideLinks'}>
+                    <li>Home</li>
+                    <li>New</li>
+                    <li>Popular</li>
+                    <li>Trending</li>
+                    <li>Categories</li>
+                </ul>
+            <img onClick={()=> setShowMenu(prevShowMenu => !prevShowMenu)} className="mobile-menu" src={`${process.env.PUBLIC_URL}/assets/images/${showMenu ? 'icon-menu-close.svg':'icon-menu.svg'}`} alt="" />
         </nav>
     )
 }
